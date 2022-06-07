ec8eb954bb396df2699918246b1bd6fc1f16cbad
Tom Coleman <tom@chromatic.com>
2022-06-07 14:51:11 +1000

    Properly go to 6.3

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,15 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react"
-}
+  refs: {
+    'react-ts': {
+      url: 'http://localhost:9011',
+    },
+  },
+  logLevel: 'debug',
+};
